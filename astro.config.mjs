b4cc47f9e6d65b3902ddb632a7a979008f3ed135
ShikatanDesu <sakurano_akira@outlook.com>
2025-08-13 20:04:59 +0800

    今天第四次小修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "sakuaki.eu.org",
+	site: "https://sakuaki.eu.org/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
